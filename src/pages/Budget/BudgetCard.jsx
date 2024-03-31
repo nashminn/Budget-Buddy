c154346d84eb230fa483861680ebb6b045429a24
nashminn <nawar.nashmin@gmail.com>
2024-03-31 10:20:48 +0600

    budget last minute

--- src/pages/Budget/BudgetCard.jsx
+++ src/pages/Budget/BudgetCard.jsx
@@ -1,13 +1,31 @@
 import { Card, CardContent, Typography } from '@mui/material'
 import React, { useEffect, useState } from 'react'
-import { deleteBudget } from '../../API/services'
+import { deleteBudget, getTransactionList, getTransactionListByMonth } from '../../API/services'
 import DeleteDialog from '../../components/DeleteDialog'
 
 export const BudgetCard = ({budget, resetCounter, setResetCounter}) => {
   const [filled, setFilled] = useState(0)
+  const [spent, setSpent] = useState(0)
+  const [remAmount, setRemAmount] = useState(0)
+
+  const isSameMonth = (d1, d2)=>{
+    return (d1.getFullYear() === d2.getFullYear() && d1.getMonth() === d2.getMonth())
+  }
 
   useEffect(() => {
-
+    const all = getTransactionListByMonth(new Date(budget.created))
+    const filtered = all.filter((x) => {
+        if(Number(x.type) === 1 ) return false
+        if(!isSameMonth(new Date(), new Date(x.created))) return false
+        if(!x.category.includes(budget.category)) return false
+        return true
+    })
+    const spent = filtered.reduce((acc, curr) => {
+        acc = Number(acc)+ Number(curr.amount)
+        return acc
+    }, 0)
+    setSpent(spent)
+    setRemAmount(Number(budget.amount) - Number(spent))
   }, [])
 
   return (
@@ -17,8 +35,17 @@ export const BudgetCard = ({budget, resetCounter, setResetCounter}) => {
                 {budget.category}
             </Typography>
             <Typography variant="body1">
-                {budget.amount}
+                Amount: {budget.amount}
             </Typography>
+
+            <Typography variant="body1">
+                Spent: {spent} 
+            </Typography>
+
+            <Typography variant="body1">
+                Remaining: {remAmount}
+            </Typography>
+            
 
             <div style={{ position: 'absolute', top: 5, right: 5 }}>  
                 <DeleteDialog showIcon={true} deleteHandle={deleteBudget} id={budget.id} 
